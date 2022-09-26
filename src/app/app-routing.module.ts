adb4af985f3fb6669098e7ff42b5d31fc44e60f6
Daniel-Ope06 <opeoluwaoyedeji06@gmail.com>
2022-09-26 20:25:51 +0100

    set up navigation

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,15 +1,20 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AboutComponent } from './components/about/about.component';
+import { ContactComponent } from './components/contact/contact.component';
 import { HomeComponent } from './components/home/home.component';
 import { MenuComponent } from './components/menu/menu.component';
+import { ServicesComponent } from './components/services/services.component';
 import { TeamComponent } from './components/team/team.component';
 
 const routes: Routes = [
+  {path:'home', title: 'Foodle App', component: HomeComponent},
+  {path:'menu', title: 'Foodle Menu', component: MenuComponent},
+  {path:'about', title: 'Foodle About', component: AboutComponent},
+  {path:'services', title: 'Foodle Services', component: ServicesComponent},
+  {path:'team', title: 'Foodle Team', component: TeamComponent},
+  {path:'contact', title: 'Foodle Contact', component: ContactComponent},
   {path:'', redirectTo: 'home', pathMatch: 'full'},
-  {path:'home', component: HomeComponent},
-  {path:'menu', component: MenuComponent},
-  {path:'team', component: TeamComponent},
-  //{path:'checkout', component: }
 ];
 
 RouterModule.forRoot(routes, {scrollPositionRestoration: 'enabled'})
--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -7,12 +7,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NavbarComponent implements OnInit {
   navbarLinks = [
-    {label: "Home",     isActive: true,  routerLink: "home"      },
-    {label: "Menu",     isActive: false,  routerLink: "menu"     },
-    {label: "About",    isActive: false,  routerLink: "about"    },
-    {label: "Services", isActive: false,  routerLink: "services" },
-    {label: "Team",     isActive: false,  routerLink: "team"     },
-    {label: "Contanct", isActive: false,  routerLink: "contact"  }
+    {label: "Home", isActive: true, routerLink: "home"},
+    {label: "Menu", isActive: false, routerLink: "menu"},
+    {label: "About", isActive: false, routerLink: "about"},
+    {label: "Services", isActive: false, routerLink: "services"},
+    {label: "Team", isActive: false, routerLink: "team"},
+    {label: "Contact", isActive: false, routerLink: "contact"},
   ];
 
   constructor() { }

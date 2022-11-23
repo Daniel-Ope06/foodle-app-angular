4cea0bfc8da917bde492911584f65b8f230d63d4
Daniel-Ope22 <ope.oyedeji@oryx-wa.com>
2022-11-23 14:01:25 +0100

    Create meal-display.model.ts

---
+++ src/app/models/meal-display.model.ts
@@ -0,0 +1,13 @@
+export class MealDisplay {
+    name: string;
+    cost: string;
+    content: string;
+    img: string;
+
+    constructor(){
+        this.name = "";
+        this.cost = "₦0";
+        this.content = "";
+        this.img = "";
+    }
+}

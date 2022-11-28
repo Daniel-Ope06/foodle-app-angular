7b28b70ffcd9453586c2729a20f9e272379ce399
Daniel-Ope22 <97707320+Daniel-Ope06@users.noreply.github.com>
2022-11-28 17:34:04 +0100

    update `navbar`

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { Database } from '@angular/fire/database';
+import { MealDisplay } from 'src/app/models/meal-display.model';
+import { FoodDB } from 'src/app/services/foodDB.service';
+import { MatDialog } from '@angular/material/dialog';
+import { CartPopupComponent } from '../cart-popup/cart-popup.component';
 
 @Component({
   selector: 'app-navbar',
@@ -15,8 +20,17 @@ export class NavbarComponent implements OnInit {
     {label: "Contact", routerLink: "contact"},
   ];
 
-  constructor() { }
+  cartList: MealDisplay[];
+
+  constructor(public database: Database, public dialog: MatDialog) {
+    this.cartList = FoodDB.getCartDB(this.database);
+   }
 
   ngOnInit(): void {
+  }
+
+  openCartDialog(): void{
+    const cartDialog = this.dialog.open(CartPopupComponent);
+    console.log("click");
   }
 }
